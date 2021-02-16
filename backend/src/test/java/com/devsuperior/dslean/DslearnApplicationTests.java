2892cdd6684d6dda1e173d7e1c2f7055680efa66
Rômulo Pereira <mp.romulo@gmail.com>
2021-02-16 14:05:41 -0300

    Backend project created

---
+++ backend/src/test/java/com/devsuperior/dslean/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
